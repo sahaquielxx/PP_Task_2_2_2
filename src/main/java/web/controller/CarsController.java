3c49db3cb46627babdb1b8179d1a06c1600d7f3c
sahaquielxx <pankihoyy@icloud.com>
2024-05-09 14:45:42 +0300

    CarsController mapp

---
+++ src/main/java/web/controller/CarsController.java
@@ -0,0 +1,10 @@
+package web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/cars")
+public class CarsController {
+
+}
